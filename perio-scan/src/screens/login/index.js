b0acc67c2809bcfe2a9ce08098a403f6c419efdd
Gabriel Di Lucas <gabrieldilucas00@gmail.com>
2025-05-27 22:56:14 -0300

    Iniciando o desenvolvimento

---
+++ perio-scan/src/screens/login/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Minha Primeira Tela!</Text>
+    </View>
+  );
+}
