ed4f597bf8fa213537a79ad224dd108ca34ae44c
Gabriel Di Lucas <gabrieldilucas00@gmail.com>
2025-06-01 15:28:35 -0300

    exclusão dos arquivos antigos

--- README.md
+++
@@ -1,1 +0,0 @@
-# PerioScan-Mobile
--- perio-scan/src/screens/login/index.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function LoginScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Minha Primeira Tela!</Text>
-    </View>
-  );
-}
